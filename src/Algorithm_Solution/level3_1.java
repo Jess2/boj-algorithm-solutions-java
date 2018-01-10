8f69da0eaa39ff529b4d86f18cf6dfcf98188f18
jessie <je_ss2@naver.com>
2018-01-10 13:10:50 +0900

    Initial commit

---
+++ src/Algorithm_Solution/level3_1.java
@@ -0,0 +1,12 @@
+package Algorithm_Solution;
+
+public class level3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world!");
+
+	}
+
+}
