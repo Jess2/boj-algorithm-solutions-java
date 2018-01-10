07baef6d3d3047f4365ea98c5038301f2242bf96
jessie <je_ss2@naver.com>
2018-01-10 13:11:47 +0900

    first commit

--- src/Algorithm_Solution/level3_1.java
+++ src/Algorithm_Solution/level3_1.java
@@ -5,7 +5,7 @@ public class level3_1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello world!");
+		System.out.println("hello world! test");
 
 	}
 
