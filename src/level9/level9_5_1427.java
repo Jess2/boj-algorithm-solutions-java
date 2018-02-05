708b62a617b3ebdf565482b3ca312c93b2b79ff5
JESS2 <je_ss2@naver.com>
2018-02-05 22:41:08 +0900

    solution commit

---
+++ src/level9/level9_5_1427.java
@@ -0,0 +1,41 @@
+//BOJ level9_5_1427
+//배열을 정렬하는 것은 쉽다. 수가 주어지면, 그 수의 각 자리수를 내림차순으로 정렬해보자.
+
+package level9;
+
+import java.util.Scanner;
+
+public class level9_5_1427 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		char[] arr = input.toCharArray();
+		
+		insertionSort(arr);
+		
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i]);
+		}
+
+	}
+	
+	//삽입 정렬 
+	public static char[] insertionSort(char[] arr) {
+		for(int i=1; i<arr.length; i++) {
+			char temp = arr[i];
+			int aux = i-1;
+			
+			while(aux>=0 && arr[aux] < temp) {
+				arr[aux+1] = arr[aux];
+				aux--;
+			}
+			arr[aux+1] = temp;
+		}
+		
+		return arr;
+	}
+
+}
