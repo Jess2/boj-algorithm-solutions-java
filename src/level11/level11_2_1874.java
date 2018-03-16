f6cae54929faf4b10508b82f6a1f8f263bed4e37
jessie <je_ss2@naver.com>
2018-03-16 19:50:18 +0900

    solution commit

--- src/level11/level11_2_1874.java
+++ src/level11/level11_2_1874.java
@@ -2,10 +2,49 @@
 
 package level11;
 
-public class level11_2_1874 {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class level11_2_1874 {
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		int stackSize = sc.nextInt(); //수열의 길이를 입력받음
+		int[] arr = new int[stackSize];
+		int[] stackArr = new int[stackSize];
+		int pushItem = 1;
+		int top = -1;
+		int loc = 0;
+		List<Character> list = new ArrayList<>();  //동적 배열 생성
+		
+		for(int i=0; i<stackSize; i++) {
+			arr[i] = sc.nextInt(); //수열의 원소들을 차례대로 입력받아 배열 arr에 저장 
+		}
+		
+		while(true) {
+			if(top != -1 && stackArr[top] == arr[loc]) {
+				top--;
+				loc++;
+				list.add('-'); //pop
+			} else if(pushItem <= stackSize) {
+				stackArr[++top] = pushItem++;
+				list.add('+'); //push
+			} else {
+				break;
+			}
+		}
+	
+		if(top == -1) {
+			for(int i=0; i<list.size(); i++) {
+				System.out.println(list.get(i));
+			}
+		} else {
+			System.out.println("NO");
+		}
+			
 
 	}
 
