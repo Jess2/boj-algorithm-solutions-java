e0838d99df18284e77803077d2b3bbcb5132d1c5
jessie <je_ss2@naver.com>
2018-04-06 23:27:19 +0900

    solution commit

--- src/level14/level14_1_2747.java
+++ src/level14/level14_1_2747.java
@@ -11,10 +11,10 @@ public class level14_1_2747 {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		long input = sc.nextInt(); //입력값
-		long pre = 0;
-		long temp = 0;
-		long result = 1; //결과
+		int input = sc.nextInt(); //입력값
+		int pre = 0;
+		int temp = 0;
+		int result = 1; //결과
 		
 		for(int i=0; i<input-1; i++) {
 			temp = result;
--- src/level14/level14_2_2748.java
+++ src/level14/level14_2_2748.java
@@ -2,11 +2,27 @@
 
 package level14;
 
+import java.util.Scanner;
+
 public class level14_2_2748 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long input = sc.nextInt(); //입력값
+		long pre = 0;
+		long temp = 0;
+		long result = 1; //결과
+		
+		for(int i=0; i<input-1; i++) {
+			temp = result;
+			result = pre + result;
+			pre = temp;
+		}
+		
+		System.out.println(result);
 	}
 
 }
