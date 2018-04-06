9aecf8efaa687543fc21fa075279b3d4bc4a6a0b
jessie <je_ss2@naver.com>
2018-04-06 23:25:49 +0900

    Solution Commit

---
+++ src/level14/level14_2_2748.java
@@ -0,0 +1,12 @@
+//https://www.acmicpc.net/problem/2748
+
+package level14;
+
+public class level14_2_2748 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
