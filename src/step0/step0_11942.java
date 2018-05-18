33fb77db7be4db230ecc7b31854fa1011d78e189
jess2 <jess2.jsy@gmail.com>
2018-05-18 19:49:37 +0900

    11942 solution commit

---
+++ src/step0/step0_11942.java
@@ -0,0 +1,14 @@
+//https://www.acmicpc.net/problem/11942
+
+package step0;
+
+public class step0_11942 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("고려대학교");
+
+	}
+
+}
