e5d5a56e8f7c8c9e0c69e1cd5462e2c9a0013c8f
jess2 <jess2.jsy@gmail.com>
2018-05-21 21:48:52 +0900

    2163 solution commit

---
+++ src/step0/step0_2163.java
@@ -0,0 +1,21 @@
+//https://www.acmicpc.net/problem/2163
+
+package step0;
+
+import java.util.Scanner;
+
+public class step0_2163 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+		
+		System.out.println(n * (m-1) + (n-1));
+
+	}
+
+}
